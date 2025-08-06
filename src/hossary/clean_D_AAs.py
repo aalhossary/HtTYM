11e74e142c837bcc4f55a7e49d2bbf980b4cd6c8
Amr ALHOSSARY <amr_alhossary@hotmail.com>
2025-08-06 13:24:06 -0400

    unify Windows & Linux

--- src/hossary/clean_D_AAs.py
+++ src/hossary/clean_D_AAs.py
@@ -1,7 +1,12 @@
+from pathlib import Path
 import pandas as pd
-import numpy as np
+import  os
 
-df = pd.read_excel(r"C:\Users\aalhossary\OneDrive - wesleyan.edu\HtTYM\TPDB\main.xlsx")
+data_folder = Path(r"C:\Users\aalhossary\OneDrive - wesleyan.edu\HtTYM\TPDB" if os.name == 'nt' else '/smithlab/home/aalhossary/HtTYM/TPDB')
+datafile1 = "main.xlsx"
+datafile2 = "main-D_GE5.xlsx"
+
+df = pd.read_excel(data_folder/datafile1)
 print(df.shape, len(df), df.columns)
 files_with_DA_AA = [100001, 120439, 155122, 155871, 157435, 157543, 158532]
 
@@ -16,4 +21,4 @@ print(df.shape, len(df))
 df.drop(df[df['Sequence'].str.contains('[a-z]', regex=True)].index, inplace=True)
 print(df.shape, len(df))
 
-df.to_excel(r"C:\Users\aalhossary\OneDrive - wesleyan.edu\HtTYM\TPDB\main-D_GE5.xlsx", index=False)
+df.to_excel(data_folder/datafile2, index=False)
